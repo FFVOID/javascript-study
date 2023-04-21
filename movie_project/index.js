94df28c4886c4abe9fc6d45d48b9ffe7c485eec5
alive1020 <961229y@naver.com>
2023-04-21 18:09:51 +0900

    up

--- movie_project/index.js
+++ movie_project/index.js
@@ -39,7 +39,7 @@ $.ajax({
     $(".poster img ").css("border-radius", "10px");
     $(".main_view_title").text(`${title}`);
     $(".info_text1").text(`${original_title}`);
-    $(".info_text2").text(`장르   ${genres}/${genres1}/${genres2}`);
+    $(".info_text2").text(`장르   ${genres} / ${genres1} / ${genres2}`);
     $(".info_text3").text(`개봉날짜 ${release_date}`);
     $(".info_text4").text(`러닝타임 ${runtime}분`);
     $(".info_text5").text(`평점 ${vote_average}`);
